2dd5656e80c49ae5a9301d34bf450fe046870b02
Jevgan <ayevhenii@rcit.ukr.education>
2023-06-02 00:26:55 +0300

    Добавьте файлы проекта.

---
+++ xo/Program.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace xo
+{
+    internal class Program
+    {
+        public static string x= "x";
+        public static string o = "o"; 
+        public static bool MyArrayIsntNull(string[] myArray)
+        {
+            for (int i = 0; i < myArray.Length; i++)
+            {
+                if (myArray[i] != null)
+                {
+                    return true;
+                }
+            }
+            Console.WriteLine(  "U win");
+            return false;
+        }
+        
+        static void Main( )
+        { 
+            int input;
+            string[] myArray = new string[9];
+            Random rnd = new Random();
+             
+            do
+            {
+                
+                //printing the array 
+                    for (int i = 0; i < myArray.Length; i++)
+                    { 
+                            Console.Write($" [  {myArray[i]}  ] ");
+                        if (i == 2 || i == 5 || i == 8)
+                            Console.WriteLine();
+                    }
+                input = int.Parse(Console.ReadLine());
+
+                    for (int i = 0; i < myArray.Length; i++)
+                    {
+                        if (input == i)
+                        {
+                            myArray[i] = x;
+                            Console.Write($" [  {myArray[i]}  ] ");
+                        }
+                        else
+                            Console.Write($" [  {myArray[i]}  ] ");
+                        if (i == 2 || i == 5 || i == 8)
+                            Console.WriteLine();
+                    }
+
+
+                 
+                Console.ReadLine();
+                Console.Clear(); 
+                 
+            } while (MyArrayIsntNull(myArray));               
+        }
+        
+    }
+}
